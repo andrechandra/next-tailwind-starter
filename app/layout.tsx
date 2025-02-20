8855ae45404abb98dde1b008c3d1779df69869f9
Andre Chandra Putra <andrechandra4817@gmail.com>
2025-02-20 09:59:11 +0700

    Updated globals.css and test commit husky

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import { Inter, Poppins, Merriweather } from 'next/font/google'
